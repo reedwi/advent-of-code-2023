ae1b8802ed9438d4b9679e6ed88de98cc1a70156
reedwi <riandolo103@gmail.com>
2023-12-04 09:01:24 -0500

    day 4

---
+++ 4/4.py
@@ -0,0 +1,48 @@
+from collections import defaultdict
+with open('4/4.txt', 'r') as file:
+    values = [line.strip() for line in file]
+
+def part_one():
+    total_points = []
+    for line in values:
+        remove_card_num = line.split(':')[1]
+        split_card = remove_card_num.split('|')
+        cards = [[num for num in card.strip().split(' ') if num] for card in split_card]
+        
+        my_card, winning_nums = cards[0], cards[1]
+        count = 0
+        for num in my_card:
+            if num in winning_nums:
+                count += 1
+        if count > 0:
+            total_points.append(2 ** (count-1))
+    return sum(total_points)
+
+def part_two():
+    winning_count = defaultdict(int)
+    collected_cards = defaultdict(int)
+    for i, line in enumerate(values, start=1):
+        remove_card_num = line.split(':')[1]
+        split_card = remove_card_num.split('|')
+        cards = [[num for num in card.strip().split(' ') if num] for card in split_card]
+        
+        my_card, winning_nums = cards[0], cards[1]
+        count = 0
+        for num in my_card:
+            if num in winning_nums:
+                count += 1
+        winning_count[i] = count
+        collected_cards[i] = 1
+
+    for i in range(1, len(values) + 1):
+        count = winning_count[i]
+        for j in range(1, count + 1):
+            next_card = i + j
+            if next_card <= len(values):
+                collected_cards[next_card] += collected_cards[i]
+
+    return sum(collected_cards.values())
+
+
+print(part_one())
+print(part_two())
